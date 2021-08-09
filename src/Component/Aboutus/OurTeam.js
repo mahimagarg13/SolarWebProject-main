afb64a958a5eee5101cd9d8e9cd4fc513b913fc5
Mahima garg <mahimagarg832@gmail.com>
2021-08-09 14:13:23 +0530

    kk

--- src/Component/Aboutus/OurTeam.js
+++ src/Component/Aboutus/OurTeam.js
@@ -2,12 +2,12 @@
 function OurTeam(params) {
     return(
 <div className="container ">
-    <div className="main">
+    <div className="main ">
                 {/* <h4 className="mt text-center">Our Team</h4> */}
         
         <h1 className="heading text-center">Our Team</h1>
         <hr />
-    <p className="text-center">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our...
+    <p className="text-center ">You will find the latest information about us on this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our...
 
 Website. We wish you a good day! You will...Find the latest information about us...On this page. Our company is constantly evolving and growing. We provide wide range of services. Our mission is to provide best solution that helps everyone. If you want to contact us, please fill the contact form on our website. We wish you a good day! You will find the latest information about us on this page. Our...
 
