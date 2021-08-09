20d447e041748f2218a98dd3fce00ee55c307d63
Mahima garg <mahimagarg832@gmail.com>
2021-08-09 15:44:25 +0530

    jv

--- src/Component/Aboutus/Mission.js
+++ src/Component/Aboutus/Mission.js
@@ -6,12 +6,12 @@ function OurMission() {
             <div className="mission">
                 <p className="heading"> Our Mission</p>
                 <hr />
-                <p>  In 2012 Nyasa Energy System was establised keeping in mind the needs of the consumers in the upcomming era. Nyasa Energy System delivers various solutions to its clients which includes </p>
+                <p className="pp">  In 2012 Nyasa Energy System was establised keeping in mind the needs of the consumers in the upcomming era. Nyasa Energy System delivers various solutions to its clients which includes </p>
             </div>
             <div className="vission"><img className="mission_img" src="/image/ser.jpeg"/></div>
             <div className="mission" > <p className="heading"> Our Vission</p>
             <hr />
-            <p>  In 2012 Nyasa Energy System was establised keeping in mind the needs of the consumers in the upcomming era. Nyasa Energy System delivers various solutions to its clients which includes </p>
+            <p className="pp">  In 2012 Nyasa Energy System was establised keeping in mind the needs of the consumers in the upcomming era. Nyasa Energy System delivers various solutions to its clients which includes </p>
 
             </div>
 
