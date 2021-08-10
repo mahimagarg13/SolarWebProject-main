3f15ae84433cf9d5d78648aefce37282c031859e
Mahima garg <mahimagarg832@gmail.com>
2021-08-10 18:15:52 +0530

    lasttt

--- src/Component/Newsletter.js
+++ src/Component/Newsletter.js
@@ -1,5 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+const ColoredLine = ({ color }) => (
+    <hr
+      style={{
+        color: color,
+        backgroundColor: color,
+        height: "0.1px",
+        width: "100%",
+        margin: 0
+      }}
+    />
+  );
 function Newsletter() {
     return (
         <div className="nl">
@@ -12,12 +22,13 @@ function Newsletter() {
                         <input type="text" className="form-control form" placeholder="Enter your email" aria-label="Recipient's username" aria-describedby="button-addon2" />
                         <button className="btn  btn-outline-success" type="button" id="button-addon2">Subscribe Now</button>
                     </div>
+
                 </div>
     
 
             </div>
-        
-           
+            <ColoredLine color="gray" />
+
 
         </div>
         
